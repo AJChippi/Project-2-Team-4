6c614717185524007444ee60c80692d20d5d4614
Anthony Chippi <chippi.anthony@gmail.com>
2022-11-16 18:57:20 -0500

    front page and also added SettingsActivity and GameActivity

--- app/src/main/java/com/example/project_2_team_4/MainActivity.java
+++ app/src/main/java/com/example/project_2_team_4/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.project_2_team_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSettings, btnNewGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSettings = findViewById(R.id.btnSettings);
+        btnNewGame = findViewById(R.id.btnNewGame);
+
+
+        btnSettings.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        });
+
+        btnNewGame.setOnClickListener(v -> {
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
+        });
     }
 }
