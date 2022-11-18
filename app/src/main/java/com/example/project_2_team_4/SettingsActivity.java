f19274ea8fbb2d58723e906c0558b56b52c7e2a3
Rakibul Hassan <rakibulh702@gmail.com>
2022-11-18 01:22:32 -0500

    add settings input control

--- app/src/main/java/com/example/project_2_team_4/SettingsActivity.java
+++ app/src/main/java/com/example/project_2_team_4/SettingsActivity.java
@@ -2,13 +2,63 @@ package com.example.project_2_team_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+    TextView txtSeek;
+    RadioGroup radioGroup;
+    SeekBar seekBar;
+    String selectedHand;
+    Button btnSave, btnBack2Main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        txtSeek = findViewById(R.id.txtSeek);
+        radioGroup = findViewById(R.id.radioGroup);
+        btnSave = findViewById(R.id.btnSave);
+        btnBack2Main = findViewById(R.id.btnBack2Menu);
+        seekBar = findViewById(R.id.seekBar);
+
+
+        txtSeek.setText(String.valueOf(seekBar.getProgress()));
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtSeek.setText(String.valueOf(i));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        radioGroup.setOnCheckedChangeListener((radioGroup, i) -> {
+            RadioButton rdoChecked = findViewById(i);
+            selectedHand = String.valueOf(rdoChecked.getText());
+        });
+
+
+
+
+        btnSave.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        btnBack2Main.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
